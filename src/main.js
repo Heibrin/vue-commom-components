938c2085420b44ec77baa98baafee34aa199ba31
Hebrin <Hebrin>
2021-09-01 16:52:55 +0800

    ✨ feat(router): 调整router位置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 import store from './store'
 import './registerServiceWorker'
+// 引入element ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入 axios
+import axios from 'axios'
+//引入tailwind
+import '@/assets/scss/main.scss'
 
+Vue.use(ElementUI)
+Vue.use(axios)
 Vue.config.productionTip = false
 
 new Vue({
